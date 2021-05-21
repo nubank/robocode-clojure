fcc988209599570cc6f7f2f22f7b0b8cea3a5aff
Dante Cassanego <dante.cassanego@gmail.com>
2021-05-21 09:43:32 -0700

    vanilla clojure project

---
+++ test/robocode_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns robocode-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [robocode-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
